bc368564634e8f845ceaf5e555e3478fc02cb583
Stephanie Lin <85892373+linstephanie89@users.noreply.github.com>
2025-05-06 19:59:15 -0400

    df

--- frontend/src/components/DownloadModal.jsx
+++ frontend/src/components/DownloadModal.jsx
@@ -42,7 +42,7 @@ export default function DownloadModal({ show, onClose }) {
 
       // Trigger the file download
       window.open(
-        'https://docs.google.com/spreadsheets/d/1yKh-pYtRI1YjpMorI-6DthgepkrB_QSEQdB3mbfCGDY/export?format=xlsx',
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vTQJ43IgiOnaY8nC_9UrZYG19LDxl0s0CsD7TUa_3cxbD-2fZqfJ87IfSPfFzFxbSPWomp2YnM2bmcm/pubhtml/export?format=xlsx',
         '_blank'
       )
     } catch (err) {
